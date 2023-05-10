2a8399251a363d458203c533c914d2de63eaba21
Özgür Rüzgar <94163712+ozgurruzgar@users.noreply.github.com>
2023-05-10 13:23:11 +0300

    Finished AutoMapper Library.

--- FluentValidationApp.Web/Mapping/CustomerProfile.cs
+++ FluentValidationApp.Web/Mapping/CustomerProfile.cs
@@ -9,11 +9,14 @@ namespace FluentValidationApp.Web.Mapping
     {
         public CustomerProfile() 
         {
+            CreateMap<CreditCard, CustomerDto>();
             //ReverseMap Function is make mapping for reverse.
-            CreateMap<Customer, CustomerDto>()
+            CreateMap<Customer, CustomerDto>().IncludeMembers(x=>x.CreditCard)
                 .ForMember(dest => dest.İsim, opt => opt.MapFrom(x => x.Name))
                 .ForMember(dest => dest.Eposta, opt => opt.MapFrom(x => x.Email))
-                .ForMember(dest => dest.Yaş, opt => opt.MapFrom(x => x.Age));
+                .ForMember(dest => dest.Yaş, opt => opt.MapFrom(x => x.Age))
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(x => x.GetFullName()));
+             //.ForMember(dest => dest.CreditCardNumber, opt => opt.MapFrom(x => x.CreditCard.Number));
         }
     }
 }
