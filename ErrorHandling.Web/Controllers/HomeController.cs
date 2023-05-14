2234ed10f3337328187c3695cc6a18d657401a6f
Özgür Rüzgar <94163712+ozgurruzgar@users.noreply.github.com>
2023-05-14 22:38:46 +0300

    Intro to Error Handling.

---
+++ ErrorHandling.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using ErrorHandling.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ErrorHandling.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //throw new Exception("An error occured while connect to Database.");
+            int num1 = 5;
+            int num2 = 0;
+            int result = num1 / num2;
+            return View(result);
+        }
+
+        public IActionResult Privacy()
+        {
+            throw new FileNotFoundException();
+            return View();
+        }
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            ViewBag.path = exception.Path;
+            ViewBag.message = exception.Error.Message;
+
+
+            return View();
+        }
+    }
+}
